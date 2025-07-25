570ec3c618f4af2d07dfd4b3f98bc45b49262a40
depaula1993 <116135305+depaula1993@users.noreply.github.com>
2025-07-25 17:04:36 -0300

    json online

--- scripts/buscarArquivos.js
+++ scripts/buscarArquivos.js
@@ -1,6 +1,7 @@
 export async function buscaArquivos() {
 
-    const dados = await fetch("http://localhost:3000/arquivos");
+    //const dados = await fetch("http://localhost:3000/arquivos");
+    const dados = await fetch("db.json");
     const  dadosTraduzidos = await dados.json();
 
      return dadosTraduzidos 
