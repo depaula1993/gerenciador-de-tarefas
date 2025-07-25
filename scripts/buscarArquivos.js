3b2d40537f0b375c1fc2f10a56c107888fbaa605
depaula1993 <116135305+depaula1993@users.noreply.github.com>
2025-07-25 17:12:08 -0300

    dfdfds

--- scripts/buscarArquivos.js
+++ scripts/buscarArquivos.js
@@ -2,6 +2,7 @@ export async function buscaArquivos() {
 
     //const dados = await fetch("http://localhost:3000/arquivos");
     const dados = await fetch("db.json");
+    console.log(dados);
     const  dadosTraduzidos = await dados.json();
 
      return dadosTraduzidos 
